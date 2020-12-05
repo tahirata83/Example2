455f02953daf2a14ea8384584ad3e7615d5c9a9c
tahirata83 <tahiratayev@ichc.ro>
2020-12-06 01:41:45 +0200

    commit 3

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,10 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("test1");
+        dene();
+    }
+
+    public static void dene(){
+        System.out.println("ata");
     }
 }
